f6d8a43a9c59bfa3fc3423710146407f58f30b0a
SendSi <389585111@qq.com>
2023-06-05 22:57:15 +0800

    初始化 抄

---
+++ Assets/Scripts/AniBG.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+#region << 脚 本 注 释 >>
+//作  用:    AniBG
+//作  者:    曾思信
+//创建时间:  2022/
+#endregion
+
+
+public class AniBG : MonoBehaviour
+{
+    Material material;
+    public Vector2 speed;
+    Vector2 movement = Vector2.one;
+    private void Start()
+    {
+        material = this.GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        movement += Time.deltaTime * speed;
+        material.mainTextureOffset = movement;
+    }
+}
+

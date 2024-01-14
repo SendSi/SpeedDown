2710a070ed647061d38580946220c0d9183f709f
SendSi <389585111@qq.com>
2024-01-14 22:52:58 +0800

    先抄着

---
+++ Assets/Scripts/FanPlatform.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class FanPlatform : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = this.GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+          _animator.Play("base3_run");
+        }
+    }
+}
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private Vector3 moveMent;
+    public float speed;
+    private GameObject topLine;
+    private void Update()
+    {
+        topLine = GameObject.Find("topLine");
+        MovePlatform();
+        moveMent.y = speed;
+    }
+
+    private void MovePlatform()
+    {
+        transform.position += moveMent * Time.deltaTime;
+        if (transform.position.y >= topLine.transform.position.y)
+        {
+            Destroy(this.gameObject);
+        }
+       
+    }
+}
